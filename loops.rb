ee4aa56634da785dda1e25dd62e90d11ce7889eb
myxabyxe <m.smelyanskaya@gmail.com>
2015-07-01 16:59:32 -0400

    first version of beer song

---
+++ loops.rb
@@ -0,0 +1,7 @@
+n = 99
+while (n > 0)
+	puts n.to_s + " bottles of beer on the wall," + n.to_s + " bottles of beer"
+	n =  n-1
+	puts "Take one down and pass it around," + n.to_s + " of beer"
+
+end
